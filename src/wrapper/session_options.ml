f84db3b4a98cd38089e215398fe9816bbfbcc998
Tianyi Song <tianyi.song@ahrefs.com>
2023-10-02 10:33:02 +0000

    Use Option.iter

--- src/wrapper/session_options.ml
+++ src/wrapper/session_options.ml
@@ -8,25 +8,14 @@ let create ?inter_op_num_threads ?intra_op_num_threads ?cuda_options () =
     set_inter_op_num_threads t ~threads:(Some threads));
   Option.iter intra_op_num_threads ~f:(fun threads ->
     set_intra_op_num_threads t ~threads:(Some threads));
-  let () =
-    Option.iter cuda_options ~f:(fun opt -> append_execution_provider_cuda t opt)
-  in
-  (* match cuda_options with
-    | None -> ()
-    | Some opt ->
-      (try append_execution_provider_cuda t opt with
-       | Failure err -> Stdio.prerr_endline @@ "Warning: failed to add CUDA EP: " ^ err) *)
+  Option.iter cuda_options ~f:(fun opt -> append_execution_provider_cuda t opt);
   t
 
 let create_cuda_options_exn ?device_id () =
   (* https://onnxruntime.ai/docs/execution-providers/CUDA-ExecutionProvider.html#configuration-options *)
   let t = create_cuda_options () in
   let kvs = ref [] in
-  let () =
-    match device_id with
-    | None -> ()
-    | Some id -> kvs := ("device_id", Int.to_string id) :: !kvs
-  in
+  Option.iter device_id ~f:(fun id -> kvs := ("device_id", Int.to_string id) :: !kvs);
   update_cuda_options t !kvs;
   t
 
